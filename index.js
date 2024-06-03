73e781f808231826ba12fcf60c03ea24c8aa1f96
Xrider-15 <171270040+Xrider-15@users.noreply.github.com>
2024-06-03 20:06:49 +0200

    at the moment doesn't work

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const anExampleVariable = "Hello World";
-console.log(anExampleVariable);
-import { MatrixAuth } from "matrix-bot-sdk";
-// This will be the URL where clients can reach your homeserver. Note that this might be different
-// from where the web/chat interface is hosted. The server must support password registration without
-// captcha or terms of service (public servers typically won't work).
-const homeserverUrl = "https://example.org";
-const auth = new MatrixAuth(homeserverUrl);
-const client = await auth.passwordRegister("username", "password");
-console.log("Copy this access token to your bot's config: ", client.accessToken);
+import { MatrixClient, SimpleFsStorageProvider, AutojoinRoomsMixin, } from "matrix-bot-sdk";
+const homeserverUrl = "https://chat.soziale.cloud";
+const accessToken = "syt_eXVt_OjKPptTMfHjfRkGnPOwo_1ZP35p";
+const storage = new SimpleFsStorageProvider("store.json");
+const client = new MatrixClient(homeserverUrl, accessToken, storage);
+AutojoinRoomsMixin.setupOnClient(client);
+// Before we start the bot, register our command handler
+client.on("room.message", handleCommand);
+// Now that everything is set up, start the bot. This will start the sync loop and run until killed.
+client.start().then(() => console.log("Bot started!"));
+// This is the command handler we registered a few lines up
+async function handleCommand(roomId, event) {
+    // Don't handle unhelpful events (ones that aren't text messages, are redacted, or sent by us)
+    if (event['content']?.['msgtype'] !== 'm.text')
+        return;
+    if (event['sender'] === await client.getUserId())
+        return;
+    // Check to ensure that the `!hello` command is being run
+    const body = event['content']['body'];
+    if (!body?.startsWith("!hello"))
+        return;
+    // Now that we've passed all the checks, we can actually act upon the command
+    await client.replyNotice(roomId, event, "Hello world!");
+}
